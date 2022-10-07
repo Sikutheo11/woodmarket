93a4aac692cc2e4058a95271624094e7bf53e901
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2022-10-07 04:00:24 -0800

    Google Autocomplete field

--- accounts/context_processors.py
+++ accounts/context_processors.py
@@ -1,4 +1,7 @@
+# from urllib.parse import uses_relative
+# from accounts.models import UserProfile
 from vendor.models import Vendor
+from django.conf import settings
 
 def get_vendor(request):
     try:
@@ -6,3 +9,10 @@ def get_vendor(request):
     except:
         vendor = None
     return dict(vendor = vendor)
+
+def get_google_api(request):
+    return {'GOOGLE_API_KEY': settings.GOOGLE_API_KEY}
+
+
+# def get_paypal_client_id(request):
+#     return {'PAYPAL_CLIENT_ID': settings.PAYPAL_CLIENT_ID}
