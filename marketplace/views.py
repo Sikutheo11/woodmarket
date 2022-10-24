7d36add26322a9f21a97a382d0e31b85c1fb29a2
Theoneste SIKUBWABO <sikutheo11@gmail.com>
2022-10-24 02:00:25 -0800

    Cart functionality

--- marketplace/views.py
+++ marketplace/views.py
@@ -2,12 +2,12 @@ from django.http import HttpResponse, JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render
 
 from accounts.models import UserProfile
-# from .context_processors import get_cart_counter, get_cart_amounts
+from .context_processors import get_cart_counter, get_cart_amounts
 from products.models import Category, ProductItem
 
 from vendor.models import Vendor
 from django.db.models import Prefetch
-# from .models import Cart
+from .models import Cart
 from django.contrib.auth.decorators import login_required
 # from django.db.models import Q
 
@@ -46,95 +46,95 @@ def vendor_detail(request, vendor_slug):
     # today = today_date.isoweekday()
     
     # current_opening_hours = OpeningHour.objects.filter(vendor=vendor, day=today)
-    # if request.user.is_authenticated:
-    #     cart_items = Cart.objects.filter(user=request.user)
-    # else:
-    #     cart_items = None
+    if request.user.is_authenticated:
+        cart_items = Cart.objects.filter(user=request.user)
+    else:
+        cart_items = None
     context = {
         'vendor': vendor,
         'categories': categories,
-    #     'cart_items': cart_items,
+        'cart_items': cart_items,
     #     'opening_hours': opening_hours,
     #     'current_opening_hours': current_opening_hours,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
 
-# def add_to_cart(request, food_id):
-#     if request.user.is_authenticated:
-#         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-#             # Check if the food item exists
-#             try:
-#                 fooditem = FoodItem.objects.get(id=food_id)
-#                 # Check if the user has already added that food to the cart
-#                 try:
-#                     chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
-#                     # Increase the cart quantity
-#                     chkCart.quantity += 1
-#                     chkCart.save()
-#                     return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
-#                 except:
-#                     chkCart = Cart.objects.create(user=request.user, fooditem=fooditem, quantity=1)
-#                     return JsonResponse({'status': 'Success', 'message': 'Added the food to the cart', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
-#             except:
-#                 return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
-#         else:
-#             return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+def add_to_cart(request, product_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the product item exists
+            try:
+                productitem = ProductItem.objects.get(id=product_id)
+                # Check if the user has already added that product to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, productitem=productitem)
+                    # Increase the cart quantity
+                    chkCart.quantity += 1
+                    chkCart.save()
+                    return JsonResponse({'status': 'Success', 'message': 'Increased the cart quantity', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})  
+                except:
+                    chkCart = Cart.objects.create(user=request.user, productitem=productitem, quantity=1)
+                    return JsonResponse({'status': 'Success', 'message': 'Added the product to the cart', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)}) 
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This product does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
         
-#     else:
-#         return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
 
 
-# def decrease_cart(request, food_id):
-#     if request.user.is_authenticated:
-#         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-#             # Check if the food item exists
-#             try:
-#                 fooditem = FoodItem.objects.get(id=food_id)
-#                 # Check if the user has already added that food to the cart
-#                 try:
-#                     chkCart = Cart.objects.get(user=request.user, fooditem=fooditem)
-#                     if chkCart.quantity > 1:
-#                         # decrease the cart quantity
-#                         chkCart.quantity -= 1
-#                         chkCart.save()
-#                     else:
-#                         chkCart.delete()
-#                         chkCart.quantity = 0
-#                     return JsonResponse({'status': 'Success', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)})
-#                 except:
-#                     return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in your cart!'})
-#             except:
-#                 return JsonResponse({'status': 'Failed', 'message': 'This food does not exist!'})
-#         else:
-#             return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+def decrease_cart(request, product_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            # Check if the product item exists
+            try:
+                productitem = ProductItem.objects.get(id=product_id)
+                # Check if the user has already added that product to the cart
+                try:
+                    chkCart = Cart.objects.get(user=request.user, productitem=productitem)
+                    if chkCart.quantity > 1:
+                        # decrease the cart quantity
+                        chkCart.quantity -= 1
+                        chkCart.save()
+                    else:
+                        chkCart.delete()
+                        chkCart.quantity = 0
+                    return JsonResponse({'status': 'Success', 'cart_counter': get_cart_counter(request), 'qty': chkCart.quantity, 'cart_amount': get_cart_amounts(request)}) 
+                except:
+                    return JsonResponse({'status': 'Failed', 'message': 'You do not have this item in your cart!'})
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'This product does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
         
-#     else:
-#         return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
+    else:
+        return JsonResponse({'status': 'login_required', 'message': 'Please login to continue'})
 
 
-# @login_required(login_url = 'login')
-# def cart(request):
-#     cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
-#     context = {
-#         'cart_items': cart_items,
-#     }
-#     return render(request, 'marketplace/cart.html', context)
+@login_required(login_url = 'login')
+def cart(request):
+    cart_items = Cart.objects.filter(user=request.user).order_by('created_at')
+    context = {
+        'cart_items': cart_items,
+    }
+    return render(request, 'marketplace/cart.html', context)
 
 
-# def delete_cart(request, cart_id):
-#     if request.user.is_authenticated:
-#         if request.headers.get('x-requested-with') == 'XMLHttpRequest':
-#             try:
-#                 # Check if the cart item exists
-#                 cart_item = Cart.objects.get(user=request.user, id=cart_id)
-#                 if cart_item:
-#                     cart_item.delete()
-#                     return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted!', 'cart_counter': get_cart_counter(request), 'cart_amount': get_cart_amounts(request)})
-#             except:
-#                 return JsonResponse({'status': 'Failed', 'message': 'Cart Item does not exist!'})
-#         else:
-#             return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
+def delete_cart(request, cart_id):
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest':
+            try:
+                # Check if the cart item exists
+                cart_item = Cart.objects.get(user=request.user, id=cart_id)
+                if cart_item:
+                    cart_item.delete()
+                    return JsonResponse({'status': 'Success', 'message': 'Cart item has been deleted!', 'cart_counter': get_cart_counter(request)}) #, 'cart_amount': get_cart_amounts(request)
+            except:
+                return JsonResponse({'status': 'Failed', 'message': 'Cart Item does not exist!'})
+        else:
+            return JsonResponse({'status': 'Failed', 'message': 'Invalid request!'})
 
 
 # def search(request):
--- woodmarket/urls.py
+++ woodmarket/urls.py
@@ -18,6 +18,8 @@ from django.urls import path, include
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
+from marketplace import views as MarketplaceViews
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
@@ -25,8 +27,8 @@ urlpatterns = [
 
     path('marketplace/', include('marketplace.urls')),
 
-    # # CART
-    # path('cart/', MarketplaceViews.cart, name='cart'),
+    # CART
+    path('cart/', MarketplaceViews.cart, name='cart'),
     # # SEARCH
     # path('search/', MarketplaceViews.search, name='search'),
 
